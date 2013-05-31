2a99428b63984e5caa1dd8690a34bb82f2a24752
cmorgoth <cristian.morgoth@gmail.com>
2013-05-31 14:36:58 -0700

    including actual examples

--- TreeArray/TreeArray.cc
+++ TreeArray/TreeArray.cc
@@ -1,4 +1,43 @@
 {
+  
   gROOT->Reset();
+  
+  ifstream myfile ("Zee.txt");
+  
+  TFile* f = new TFile("treeArray.root","RECREATE");
 
+  TTree* tree = new TTree("tree","Tree Array");
+
+  int nZ;
+  int charge[500];
+  float EleE[500], ElePx[500], ElePy[500], ElePz[500];
+  
+  tree->Branch("nZ", &nZ, "nZ/I");
+  tree->Branch("charge", charge, "charge[nZ]/I");
+  tree->Branch("EleE", EleE, "EleE[nZ]/F");
+  tree->Branch("ElePx", ElePx, "ElePx[nZ]/F");
+  tree->Branch("ElePy", ElePy, "ElePy[nZ]/F");
+  tree->Branch("ElePz", ElePz, "ElePz[nZ]/F");
+  
+  int ctr = 0;
+  
+  if( myfile.is_open() ){
+    
+    while( myfile.good() ){
+      
+      myfile >> nZ >> charge[ctr] >> EleE[ctr] >> ElePx[ctr] >> ElePy[ctr] >> ElePz[ctr];
+      ctr++;
+      if( ctr == nZ ){
+	tree->Fill();
+	ctr = 0;     
+      }
+      
+    }
+    
+    myfile.close();
+    
+  }else cout << "Unable to open file"; 
+  
+  tree->Write("tree");
+  f->Close();
 }
